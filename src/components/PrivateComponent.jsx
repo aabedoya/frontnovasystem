c570d324d65d677f32b58f187b1a2080e18a4dbe
Alvaro Andres Bedoya Gomez <andresbedoyagomez@gmail.com>
2021-10-24 00:00:57 -0500

    verificar datos en el private component ROL

--- src/components/PrivateComponent.jsx
+++ src/components/PrivateComponent.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const PrivateComponent = ({ roleList, children }) => {
   const { userData } = useUser();
+  console.log('Rol cargado',userData.rol);
   console.log('datos del user data en el private component',userData);
   if (roleList.includes(userData.rol)) {
     return children;
